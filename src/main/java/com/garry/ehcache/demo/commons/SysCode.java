7612cfb7425234516bf251d792be476b115d883d
Garry <luol@citydo.com.cn>
2019-03-19 12:34:53 +0800

    '修改enum上使用lombok注解'

--- src/main/java/com/garry/ehcache/demo/commons/SysCode.java
+++ src/main/java/com/garry/ehcache/demo/commons/SysCode.java
@@ -1,38 +1,21 @@
 package com.garry.ehcache.demo.commons;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 /**
  * Author : GarryLuo
  * Date  : 2019/1/21 16:15
  * Description : 返回错误码类，不要改动码
  */
+@Getter
+@AllArgsConstructor
 public enum SysCode {
-
     SUCCESS("0000", "成功"),
     EXCEPTION("1001", "系统异常"),
     DIY_EXCEPTION("1002", "自定义系统异常");
 
     private String code;
-
     private String message;
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    SysCode(String code, String message) {
-        this.code = code;
-        this.message = message;
-    }
 }
